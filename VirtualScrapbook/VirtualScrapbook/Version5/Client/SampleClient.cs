60e9a9fc8d94aed55f8f372fc7fa02e4cc339a71
Steve Smith <steve@kentsmiths.com>
2015-06-24 23:31:32 -0700

    Added Version 5 with ReadOnlyCollection support.

---
+++ VirtualScrapbook/VirtualScrapbook/Version5/Client/SampleClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using VirtualScrapbook.Version5.Model;
+
+namespace VirtualScrapbook.Version5.Client
+{
+    public class SampleClient
+    {
+        public void DisplayBook(Book book, StringBuilder sb)
+        {
+            if (book == null)
+            {
+                throw new ArgumentNullException("Book cannot be null.");
+            }
+            sb.AppendLine(book.Title);
+            var coverImage = book.GetCoverImage();
+            sb.AppendLine("Cover Image: " + coverImage.ImagePath);
+            foreach (var image in book.ReadOnlyImages)
+            {
+                sb.AppendLine(image.ImagePath);
+            }
+        }
+
+        public void SetCoverImage(Book book, string imagePath)
+        {
+            if (book == null)
+            {
+                throw new ArgumentNullException("Book cannot be null.");
+            }
+            book.SetCoverImage(imagePath);
+        }
+    }
+}
