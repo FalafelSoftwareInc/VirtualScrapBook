3fc82eb9c367e28ed0da4816aa5767de4b0ee617
Steve Smith <steve@kentsmiths.com>
2015-06-24 22:29:19 -0700

    Initial commit

---
+++ VirtualScrapbook/VirtualScrapbook/Version1/Client/SampleClient.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text;
+using VirtualScrapbook.Version1.Model;
+
+namespace VirtualScrapbook.Version1.Client
+{
+    public class SampleClient
+    {
+        public void DisplayBook(Book book, StringBuilder sb)
+        {
+            if (book == null)
+            {
+                throw new ArgumentNullException("Book cannot be null.");
+            }
+            sb.AppendLine(book.Title);
+            if(book.Images != null)
+            {
+                foreach(var image in book.Images)
+                {
+                    sb.AppendLine(image.ImagePath);
+                }
+            }
+        }
+    }
+}
---
+++ VirtualScrapbook/VirtualScrapbook/Version1/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace VirtualScrapbook.Version1.Model
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public List<Image> Images { get; set; }
+    }
+
+    public class Image
+    {
+        public string ImagePath { get; set; }
+    }
+}
